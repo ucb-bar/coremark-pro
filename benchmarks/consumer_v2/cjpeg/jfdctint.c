cf1efbe6bf33ae51348a3dfae58bf0fbf6120bf4
Jerry Zhao <jerryz123@berkeley.edu>
2024-07-18 23:47:02 -0700

    Vectorize part of jpeg fdct

--- benchmarks/consumer_v2/cjpeg/jfdctint.c
+++ benchmarks/consumer_v2/cjpeg/jfdctint.c
@@ -1,12 +1,12 @@
 /*
-(C) 2014 EEMBC(R).  All rights reserved.                            
+(C) 2014 EEMBC(R).  All rights reserved.
 
-All EEMBC Benchmark Software are products of EEMBC 
-and are provided under the terms of the EEMBC Benchmark License Agreements.  
-The EEMBC Benchmark Software are proprietary intellectual properties of EEMBC and its Members 
-and is protected under all applicable laws, including all applicable copyright laws.  
-If you received this EEMBC Benchmark Software without having 
-a currently effective EEMBC Benchmark License Agreement, you must discontinue use. 
+All EEMBC Benchmark Software are products of EEMBC
+and are provided under the terms of the EEMBC Benchmark License Agreements.
+The EEMBC Benchmark Software are proprietary intellectual properties of EEMBC and its Members
+and is protected under all applicable laws, including all applicable copyright laws.
+If you received this EEMBC Benchmark Software without having
+a currently effective EEMBC Benchmark License Agreement, you must discontinue use.
 Please refer to LICENSE.md for the specific license agreement that pertains to this Benchmark Software.
 */
 
@@ -17,15 +17,15 @@ Please refer to LICENSE.md for the specific license agreement that pertains to t
  *
  *  EEMBC : Consumer Subcommittee
  *
- * AUTHOR : 
- *          Modified for Multi Instance Test Harness by Ron Olson, 
+ * AUTHOR :
+ *          Modified for Multi Instance Test Harness by Ron Olson,
  *          IBM Corporation
  *
  *    CVS : $Revision: 1.1 $
  *          $Date: 2004/05/10 22:36:04 $
  *          $Author: rick $
  *          $Source: D:/cvs/eembc2/consumer/cjpegv2/jfdctint.c,v $
- *          
+ *
  * NOTE   :
  *
  *------------------------------------------------------------------------------
@@ -44,8 +44,8 @@ Please refer to LICENSE.md for the specific license agreement that pertains to t
  *
  *
  *------------------------------------------------------------------------------
- * Other Copyright Notice (if any): 
- * 
+ * Other Copyright Notice (if any):
+ *
  * For conditions of distribution and use, see the accompanying README file.
  * ===========================================================================*/
 /*
@@ -199,6 +199,143 @@ jpeg_fdct_islow (DCTELEM * data)
   int ctr;
   SHIFT_TEMPS
 
+#if USE_RVV
+  /* Pass 1: process rows. */
+  /* Note results are scaled up by sqrt(8) compared to a true DCT; */
+  /* furthermore, we scale the results by 2**PASS1_BITS. */
+  /* DCTELEM * rvv_out = malloc(sizeof(DCTELEM) * DCTSIZE2); */
+  /* memcpy(rvv_out, data, sizeof(DCTELEM) * DCTSIZE2); */
+  /* dataptr = rvv_out; */
+  dataptr = data;
+
+  for (ctr = 0; ctr < DCTSIZE;) {
+    size_t vl;
+
+#define IN0 "v0"
+#define IN1 "v1"
+#define IN2 "v2"
+#define IN3 "v3"
+#define IN4 "v4"
+#define IN5 "v5"
+#define IN6 "v6"
+#define IN7 "v7"
+#define TMP0 "v8"
+#define TMP1 "v9"
+#define TMP2 "v10"
+#define TMP3 "v11"
+#define TMP4 "v12"
+#define TMP5 "v13"
+#define TMP6 "v14"
+#define TMP7 "v15"
+#define TMP10 "v16"
+#define TMP11 "v17"
+#define TMP12 "v18"
+#define TMP13 "v19"
+#define Z1 "v20"
+#define Z2 "v21"
+#define Z3 "v22"
+#define Z4 "v23"
+#define Z5 "v0"
+#define OUT0 "v24"
+#define OUT1 "v25"
+#define OUT2 "v26"
+#define OUT3 "v27"
+#define OUT4 "v28"
+#define OUT5 "v29"
+#define OUT6 "v30"
+#define OUT7 "v31"
+
+#define VOP_VV(op, vd, vs2, vs1) __asm__ volatile("v" op ".vv " vd ", " vs2 ", " vs1);
+#define VOP_VI(op, vd, vs2, rs1) __asm__ volatile("v" op ".vi " vd ", " vs2 ", " #rs1);
+#define VOP_VX(op, vd, vs2, rs1) __asm__ volatile("v" op ".vx " vd ", " vs2 ", %0" : : "r"(rs1));
+
+#define VMUL_VV(vd, vs2, vs1) VOP_VV("mul", vd, vs2, vs1)
+#define VMUL_VX(vd, vs2, rs1) VOP_VX("mul", vd, vs2, rs1)
+#define VADD_VV(vd, vs2, vs1) VOP_VV("add", vd, vs2, vs1)
+#define VADD_VX(vd, vs2, rs1) VOP_VX("add", vd, vs2, rs1)
+#define VSUB_VV(vd, vs2, vs1) VOP_VV("sub", vd, vs2, vs1)
+#define VSLL_VI(vd, vs2, rs1) VOP_VI("sll", vd, vs2, rs1)
+#define VSRA_VI(vd, vs2, rs1) VOP_VI("sra", vd, vs2, rs1)
+
+#define V_DESCALE(vr, n)                          \
+    VADD_VX(vr, vr, ONE << (n-1));                \
+    VSRA_VI(vr, vr, n);                           \
+
+    __asm__ volatile("vsetvli %0, %1, e32, m1, ta, ma" : "=r"(vl) : "r"(DCTSIZE));
+    __asm__ volatile("vlseg8e32.v v0, (%0)" : : "r"(&dataptr[ctr * DCTSIZE]));
+    VADD_VV(TMP0, IN0, IN7);
+    VSUB_VV(TMP7, IN0, IN7);
+    VADD_VV(TMP1, IN1, IN6);
+    VSUB_VV(TMP6, IN1, IN6);
+    VADD_VV(TMP2, IN2, IN5);
+    VSUB_VV(TMP5, IN2, IN5);
+    VADD_VV(TMP3, IN3, IN4);
+    VSUB_VV(TMP4, IN3, IN4);
+
+    VADD_VV(TMP10, TMP0, TMP3);
+    VSUB_VV(TMP13, TMP0, TMP3);
+    VADD_VV(TMP11, TMP1, TMP2);
+    VSUB_VV(TMP12, TMP1, TMP2);
+
+    VADD_VV(OUT0, TMP10, TMP11);
+    VSLL_VI(OUT0, OUT0, PASS1_BITS);
+    VSUB_VV(OUT4, TMP10, TMP11);
+    VSLL_VI(OUT4, OUT4, PASS1_BITS);
+
+    VADD_VV(Z1, TMP12, TMP13);
+    VMUL_VX(Z1, Z1, FIX_0_541196100);
+
+    VMUL_VX(OUT2, TMP13, FIX_0_765366865);
+    VADD_VV(OUT2, OUT2, Z1);
+    V_DESCALE(OUT2, CONST_BITS-PASS1_BITS);
+
+    VMUL_VX(OUT6, TMP12, - FIX_1_847759065);
+    VADD_VV(OUT6, OUT6, Z1);
+    V_DESCALE(OUT6, CONST_BITS-PASS1_BITS);
+
+    VADD_VV(Z1, TMP4, TMP7);
+    VADD_VV(Z2, TMP5, TMP6);
+    VADD_VV(Z3, TMP4, TMP6);
+    VADD_VV(Z4, TMP5, TMP7);
+
+    VADD_VV(Z5, Z3, Z4);
+    VMUL_VX(Z5, Z5, FIX_1_175875602);
+
+    VMUL_VX(TMP4, TMP4, FIX_0_298631336);
+    VMUL_VX(TMP5, TMP5, FIX_2_053119869);
+    VMUL_VX(TMP6, TMP6, FIX_3_072711026);
+    VMUL_VX(TMP7, TMP7, FIX_1_501321110);
+
+    VMUL_VX(Z1, Z1, - FIX_0_899976223);
+    VMUL_VX(Z2, Z2, - FIX_2_562915447);
+    VMUL_VX(Z3, Z3, - FIX_1_961570560);
+    VMUL_VX(Z4, Z4, - FIX_0_390180644);
+
+    VADD_VV(Z3, Z3, Z5);
+    VADD_VV(Z4, Z4, Z5);
+
+    VADD_VV(OUT7, TMP4, Z1);
+    VADD_VV(OUT7, OUT7, Z3);
+    V_DESCALE(OUT7, CONST_BITS-PASS1_BITS);
+
+    VADD_VV(OUT5, TMP5, Z2);
+    VADD_VV(OUT5, OUT5, Z4);
+    V_DESCALE(OUT5, CONST_BITS-PASS1_BITS);
+
+    VADD_VV(OUT3, TMP6, Z2);
+    VADD_VV(OUT3, OUT3, Z3);
+    V_DESCALE(OUT3, CONST_BITS-PASS1_BITS);
+
+    VADD_VV(OUT1, TMP7, Z1);
+    VADD_VV(OUT1, OUT1, Z4);
+    V_DESCALE(OUT1, CONST_BITS-PASS1_BITS);
+
+    __asm__ volatile("vsseg8e32.v v24, (%0)" : : "r"(&dataptr[ctr * DCTSIZE]));
+
+    ctr += vl;
+  }
+#else
+
   /* Pass 1: process rows. */
   /* Note results are scaled up by sqrt(8) compared to a true DCT; */
   /* furthermore, we scale the results by 2**PASS1_BITS. */
@@ -213,36 +350,36 @@ jpeg_fdct_islow (DCTELEM * data)
     tmp5 = dataptr[2] - dataptr[5];
     tmp3 = dataptr[3] + dataptr[4];
     tmp4 = dataptr[3] - dataptr[4];
-    
+
     /* Even part per LL&M figure 1 --- note that published figure is faulty;
      * rotator "sqrt(2)*c1" should be "sqrt(2)*c6".
      */
-    
+
     tmp10 = tmp0 + tmp3;
     tmp13 = tmp0 - tmp3;
     tmp11 = tmp1 + tmp2;
     tmp12 = tmp1 - tmp2;
-    
+
     dataptr[0] = (DCTELEM) ((tmp10 + tmp11) << PASS1_BITS);
     dataptr[4] = (DCTELEM) ((tmp10 - tmp11) << PASS1_BITS);
-    
+
     z1 = MULTIPLY(tmp12 + tmp13, FIX_0_541196100);
     dataptr[2] = (DCTELEM) DESCALE(z1 + MULTIPLY(tmp13, FIX_0_765366865),
                    CONST_BITS-PASS1_BITS);
     dataptr[6] = (DCTELEM) DESCALE(z1 + MULTIPLY(tmp12, - FIX_1_847759065),
                    CONST_BITS-PASS1_BITS);
-    
+
     /* Odd part per figure 8 --- note paper omits factor of sqrt(2).
      * cK represents cos(K*pi/16).
      * i0..i3 in the paper are tmp4..tmp7 here.
      */
-    
+
     z1 = tmp4 + tmp7;
     z2 = tmp5 + tmp6;
     z3 = tmp4 + tmp6;
     z4 = tmp5 + tmp7;
     z5 = MULTIPLY(z3 + z4, FIX_1_175875602); /* sqrt(2) * c3 */
-    
+
     tmp4 = MULTIPLY(tmp4, FIX_0_298631336); /* sqrt(2) * (-c1+c3+c5-c7) */
     tmp5 = MULTIPLY(tmp5, FIX_2_053119869); /* sqrt(2) * ( c1+c3-c5+c7) */
     tmp6 = MULTIPLY(tmp6, FIX_3_072711026); /* sqrt(2) * ( c1+c3+c5-c7) */
@@ -251,17 +388,18 @@ jpeg_fdct_islow (DCTELEM * data)
     z2 = MULTIPLY(z2, - FIX_2_562915447); /* sqrt(2) * (-c1-c3) */
     z3 = MULTIPLY(z3, - FIX_1_961570560); /* sqrt(2) * (-c3-c5) */
     z4 = MULTIPLY(z4, - FIX_0_390180644); /* sqrt(2) * (c5-c3) */
-    
+
     z3 += z5;
     z4 += z5;
-    
+
     dataptr[7] = (DCTELEM) DESCALE(tmp4 + z1 + z3, CONST_BITS-PASS1_BITS);
     dataptr[5] = (DCTELEM) DESCALE(tmp5 + z2 + z4, CONST_BITS-PASS1_BITS);
     dataptr[3] = (DCTELEM) DESCALE(tmp6 + z2 + z3, CONST_BITS-PASS1_BITS);
     dataptr[1] = (DCTELEM) DESCALE(tmp7 + z1 + z4, CONST_BITS-PASS1_BITS);
-    
+
     dataptr += DCTSIZE;        /* advance pointer to next row */
   }
+#endif
 
   /* Pass 2: process columns.
    * We remove the PASS1_BITS scaling, but leave the results scaled up
@@ -278,36 +416,36 @@ jpeg_fdct_islow (DCTELEM * data)
     tmp5 = dataptr[DCTSIZE*2] - dataptr[DCTSIZE*5];
     tmp3 = dataptr[DCTSIZE*3] + dataptr[DCTSIZE*4];
     tmp4 = dataptr[DCTSIZE*3] - dataptr[DCTSIZE*4];
-    
+
     /* Even part per LL&M figure 1 --- note that published figure is faulty;
      * rotator "sqrt(2)*c1" should be "sqrt(2)*c6".
      */
-    
+
     tmp10 = tmp0 + tmp3;
     tmp13 = tmp0 - tmp3;
     tmp11 = tmp1 + tmp2;
     tmp12 = tmp1 - tmp2;
-    
+
     dataptr[DCTSIZE*0] = (DCTELEM) DESCALE(tmp10 + tmp11, PASS1_BITS);
     dataptr[DCTSIZE*4] = (DCTELEM) DESCALE(tmp10 - tmp11, PASS1_BITS);
-    
+
     z1 = MULTIPLY(tmp12 + tmp13, FIX_0_541196100);
     dataptr[DCTSIZE*2] = (DCTELEM) DESCALE(z1 + MULTIPLY(tmp13, FIX_0_765366865),
                        CONST_BITS+PASS1_BITS);
     dataptr[DCTSIZE*6] = (DCTELEM) DESCALE(z1 + MULTIPLY(tmp12, - FIX_1_847759065),
                        CONST_BITS+PASS1_BITS);
-    
+
     /* Odd part per figure 8 --- note paper omits factor of sqrt(2).
      * cK represents cos(K*pi/16).
      * i0..i3 in the paper are tmp4..tmp7 here.
      */
-    
+
     z1 = tmp4 + tmp7;
     z2 = tmp5 + tmp6;
     z3 = tmp4 + tmp6;
     z4 = tmp5 + tmp7;
     z5 = MULTIPLY(z3 + z4, FIX_1_175875602); /* sqrt(2) * c3 */
-    
+
     tmp4 = MULTIPLY(tmp4, FIX_0_298631336); /* sqrt(2) * (-c1+c3+c5-c7) */
     tmp5 = MULTIPLY(tmp5, FIX_2_053119869); /* sqrt(2) * ( c1+c3-c5+c7) */
     tmp6 = MULTIPLY(tmp6, FIX_3_072711026); /* sqrt(2) * ( c1+c3+c5-c7) */
@@ -316,10 +454,10 @@ jpeg_fdct_islow (DCTELEM * data)
     z2 = MULTIPLY(z2, - FIX_2_562915447); /* sqrt(2) * (-c1-c3) */
     z3 = MULTIPLY(z3, - FIX_1_961570560); /* sqrt(2) * (-c3-c5) */
     z4 = MULTIPLY(z4, - FIX_0_390180644); /* sqrt(2) * (c5-c3) */
-    
+
     z3 += z5;
     z4 += z5;
-    
+
     dataptr[DCTSIZE*7] = (DCTELEM) DESCALE(tmp4 + z1 + z3,
                        CONST_BITS+PASS1_BITS);
     dataptr[DCTSIZE*5] = (DCTELEM) DESCALE(tmp5 + z2 + z4,
@@ -328,7 +466,7 @@ jpeg_fdct_islow (DCTELEM * data)
                        CONST_BITS+PASS1_BITS);
     dataptr[DCTSIZE*1] = (DCTELEM) DESCALE(tmp7 + z1 + z4,
                        CONST_BITS+PASS1_BITS);
-    
+
     dataptr++;            /* advance pointer to next column */
   }
 }
